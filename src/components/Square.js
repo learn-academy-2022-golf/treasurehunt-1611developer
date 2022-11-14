dfeb984fabcd8d447d8fd96d54a75295d32297e4
1611developer <1611.developer@gmail.com>
2022-11-14 10:37:57 -0800

    alert-index user story

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,20 @@
 import React from "react"
 
 const Square = (props) => {
+  // console.log(props.index)
+
+  const handleClick = () => {
+    // alert(props.index)
+    // invoke handleGamePlay
+    // inside pass props.index
+    props.handleGamePlay(props.index)
+  }
+
   return (
     <>
     {/* import props from square value from app.js*/}
-      <div className="square">{props.value}</div>
+    {/* onClick invokes a function */}
+      <div className="square" onClick={handleClick}>{props.value}</div>
     </>
   )
 }
