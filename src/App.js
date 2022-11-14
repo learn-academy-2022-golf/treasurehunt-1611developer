489cf66592069a3d160d3164ca66e28f499c9dc5
1611developer <1611.developer@gmail.com>
2022-11-14 11:07:58 -0800

    treasure and bomb emojis appear when clicking on randomized locations

--- src/App.js
+++ src/App.js
@@ -15,18 +15,36 @@ const App = () => {
     "?"
   ])
 
+  // store it in state
+  // initialize default value to length of array
+  const [treasureLocation, setTreasureLocation] = useState(Math.floor(Math.random() * board.length))
+  const [bombLocation, setBombLocation] = useState(Math.floor(Math.random() * board.length))
+
   // in react have to make a duplicate copy of array to update a single value in an array. Set up variable in handleGamePlay. Make complete copy of game variable in state by using spread operator.
 
   // get index from square component back into app.js
   const handleGamePlay = (index) => {
     // alert(index)
     // this step is super important. cannot modify single instance. whole array has to be modified
+    // Makes a copy of the board in state so that we can modify a single instance.
     let updatedBoard = [...board]
-
-    // this current value im clicking, i want to assign a new value to it
-    updatedBoard[index] = "🌴"
-    // console.log(updatedBoard)
-    setBoard(updatedBoard)
+    if(index === treasureLocation) {
+      // this current value im clicking, i want to assign a new value to it
+      // updating a single instance inside the array
+      updatedBoard[index] = "💎"
+      // console.log(updatedBoard)
+      //setting entire updated board to state
+      setBoard(updatedBoard)
+    } else if (index === bombLocation) {
+        // this current value im clicking, i want to assign a new value to it
+        updatedBoard[index] = "💣"
+        // console.log(updatedBoard)
+        setBoard(updatedBoard)
+    } else {
+        updatedBoard[index] = "🌴"
+        // console.log(updatedBoard)
+        setBoard(updatedBoard)
+    }
   }
 
   return (
